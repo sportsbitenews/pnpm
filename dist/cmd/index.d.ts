52eeaedb67764f0e5e6b349012143c342d31c057
zkochan <zoltan.kochan@gmail.com>
2017-09-26 21:30:33 +0300

    chore: commit dist folder

---
+++ dist/cmd/index.d.ts
@@ -0,0 +1,14 @@
+import install from './install';
+import installTest from './installTest';
+import link from './link';
+import prune from './prune';
+import run from './run';
+import store from './store';
+import uninstall from './uninstall';
+import dislink from './dislink';
+import update from './update';
+import list from './list';
+import help from './help';
+import root from './root';
+import outdated from './outdated';
+export { install, installTest, link, prune, run, store, uninstall, dislink, update, list, help, root, outdated };
